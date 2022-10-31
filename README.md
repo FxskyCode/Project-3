af8224d198cd6424a85e1bbc7e8dcc44441eda95
David Ros <fxskycode@gmail.com>
2022-10-31 22:21:21 +0100

    david

--- README.md
+++ README.md
@@ -1,1 +1,95 @@
 # Project-3
+
+<img src="/images/london.jpeg">
+
+
+
+### Goals
+The Goal for this project is to analyze startup database through mongoDB and python in order to find the best location for a new office via maps visualization graphics.
+
+
+
+What I would like to present for this project is the best location in London city for the new offices taking as a starter point the offices that we have on the database 
+
+### Querying and Cleansing
+
+I started by querying the database with 3 mainly parameters and with this making 3 dataframe to find out the countries and cities with more companies.
+
+The process ended with 3 cities with more companies.
+
+-San Franciso
+-New York
+-London
+
+Between this cities I chose London because its way closer to Spain and i feel will be less difficult set up the offices.
+
+Taking London as the chosen place I query the database to take all the companies in London, with this result I made a Dataframe
+
+Having all the companies and the office location, I request through Forsquare API what was the closest location of some establishments such as (Starbucks, School, Airport, Basketball Store, Night Club) for each companie office in London.
+
+After having all this information in a dataframe I finally added one last column for Scores , score is a small formula to give each office a score on the distance to each establishment and the importance.
+
+In possession of all this information I chose the top 5 locations.
+
+![](images/graph1/../s2.jpeg)
+
+
+### Analysis and Visualization
+
+In order to see the 5 best locations for our offices I plotted the maps below. As we can see in the images below all the offices have a starbucks nearby,having a starbucks close had a high weight in the choice of the office (coffee is life).
+
+
+#### First Office
+![](images/graph1/../s3.jpeg)
+
+
+
+#### Second Office
+![](images/graph1/../s5.jpeg)
+
+
+
+#### Third Office
+![](images/graph1/../s6.jpeg)
+
+
+
+#### Fourth Office
+![](images/graph1/../s8.jpeg)
+
+
+
+![](images/graph1/../graph4.jpeg)
+
+#### Fifth Office
+
+
+![](images/graph1/../s9.jpeg)
+
+
+
+
+### Conclusion
+
+Giving high weight to the airport can be misleading because 90% are outside of the city , but the other places we want close can be found easy in the city center, also starbucks greatly reduce other possible locations, changing starbucks for any decent coffee bar can give better places.
+
+
+![](images/graph1/../chess2.jpeg)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/functions.py
@@ -0,0 +1,136 @@
+from pymongo import MongoClient
+import pandas as pd
+import os
+import requests
+import json
+from dotenv import load_dotenv
+from bs4 import BeautifulSoup
+import re
+import geopandas as gpd
+from cartoframes.viz import Map, Layer, popup_element
+import numpy as np
+
+import folium
+from folium import Choropleth, Circle, Marker, Icon, Map
+from folium.plugins import HeatMap, MarkerCluster
+
+def extract_company(filter_, c):
+    projection = {"_id":0, "name":1, "category_code":1, "tag_list":1, "total_money_raised":1, "offices.city":1, "offices.state_code": 1, "offices.country_code":1,"offices.latitude": 1, "offices.longitude": 1}
+    list_ = list(c.find(filter_, projection))
+    
+    df = pd.DataFrame(list_).explode("offices").reset_index(drop=True)
+    df = pd.concat([df, df["offices"].apply(pd.Series)], axis=1).reset_index(drop=True)
+    df = df.drop(["offices"], axis = 1)
+    df = df[df["city"]!=""]
+    
+    return df
+
+
+def foursquare_cat (category, name, df): # v2
+    response_list = []
+    distance = []
+    lat = []
+    lon = []
+    
+    for i in range(len(df)):
+        url = f"https://api.foursquare.com/v3/places/search?ll={df['latitude'][i]}%2C{df['longitude'][i]}&categories={category}&limit=1"
+        headers = {"accept": "application/json", "Authorization": token_fsq}
+        response = requests.get(url, headers=headers).json()
+        
+        response_list.append(response)
+        
+    for x in range(len(response_list)):
+        try:
+            distance.append(response_list[x]["results"][0]["distance"])
+        except:
+            distance.append(None)
+            
+
+    for x in range(len(response_list)):
+        try:
+            lat.append(response_list[x]["results"][0]["geocodes"]["main"]["latitude"])
+        except:
+            lat.append(None)
+            
+
+    for x in range(len(response_list)):
+        try:
+            lon.append(response_list[x]["results"][0]["geocodes"]["main"]["longitude"])
+
+
+        except:
+            lon.append(None)
+
+
+            
+
+        
+    df[f"{name}_dist"] = distance
+    df[f"{name}_lat"] = lat
+    df[f"{name}_lon"] = lon
+
+
+    
+    return df
+
+
+def foursquare_query (query, df): # v2
+    response_list = []
+    distance = []
+    lat = []
+    lon = []
+    
+    for i in range(len(df)):
+        url = f"https://api.foursquare.com/v3/places/search?query={query}&ll={df['latitude'][i]}%2C{df['longitude'][i]}&sort=DISTANCE&limit=1"
+        headers = {"accept": "application/json", "Authorization": token_fsq}
+        response = requests.get(url, headers=headers).json()
+        
+        response_list.append(response)
+
+        
+        
+    for x in range(len(response_list)):
+        try:
+            distance.append(response_list[x]["results"][0]["distance"])
+        except:
+            distance.append(None)
+            
+
+    for x in range(len(response_list)):
+        try:
+            lat.append(response_list[x]["results"][0]["geocodes"]["main"]["latitude"])
+        except:
+            lat.append(None)
+            
+
+    for x in range(len(response_list)):
+        try:
+            lon.append(response_list[x]["results"][0]["geocodes"]["main"]["longitude"])
+
+
+        except:
+            lon.append(None)
+        
+    df[f"{query}_dist"] = distance
+    df[f"{query}_lat"] = lat
+    df[f"{query}_lon"] = lon
+    
+    
+    return df
+
+def distance(df):
+    score = []
+    for i in range(len(df)):
+        airport = df["airport_dist"][i]
+        starbucks = df["starbucks_dist"][i]
+        night_club = df["n_club_dist"][i]
+        basket = df["b_stadium_dist"][i]
+        school = df["school_dist"][i]
+        total_score = (1/airport * 0.30)*100 + (1/starbucks * 0.25)*100 + (1/night_club * 0.20)*100 + (1/basket * 0.15)*100 + (1/school * 0.10)*100
+        score.append(total_score)
+        
+    df["score"] = score
+    
+    return df
+            
+            
